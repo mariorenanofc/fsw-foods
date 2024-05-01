f4fe172522f5da8a1a8d9d719a27bb6e8d3dd584
mariorenanofc <mariorenan25@gmail.com>
2024-05-01 16:14:03 -0300

    feat: add  category item and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
@@ -5,8 +6,12 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="px-05 pt-6">
+      <div className="px-5 pt-6">
         <Search />
+      </div>
+
+      <div className="px-5">
+        <CategoryList />
       </div>
     </>
   );
