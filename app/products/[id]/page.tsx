62c7620352474ad20cb323d349176fe064b3cd0d
mariorenanofc <mariorenan25@gmail.com>
2024-05-12 13:57:19 -0300

    feat: ensure juices from product restaurant are listed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -18,20 +18,23 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product?.restaurant.id,
       },
     },
     include: {
       restaurant: true,
     },
   });
-
-  if (!product) {
-    return notFound();
-  }
 
   return (
     <div>
